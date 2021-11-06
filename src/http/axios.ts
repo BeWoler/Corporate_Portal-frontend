a0fe0c1933309eccaa8b0a3b6ee061004a666cb6
BeWoler <bewoler.del@gmail.com>
2021-11-07 02:47:24 +0300

    created axios request

---
+++ src/http/axios.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const API_URL = "http://localhost:3010/api"
+
+const api = axios.create({
+  withCredentials: true,
+  baseURL: API_URL
+})
+
+api.interceptors.request.use((config) => {
+  config.headers.Autthorization = `Bearer ${localStorage.getItem('token')}`
+  return config;
+})
+
+export default api;
