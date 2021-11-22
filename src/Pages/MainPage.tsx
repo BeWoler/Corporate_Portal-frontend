0d77ad256c4fa56f57b6a4184f954a48e1bf9eb5
BeWoler <bewoler.del@gmail.com>
2021-11-22 03:16:07 +0300

    edited main page, user profile, settings page

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,14 +1,26 @@
 import { FC, useContext } from "react";
 import { Context } from "../index";
+import "../styles/mainPage.css"
 
 const MainPage: FC = () => {
   const { store } = useContext(Context);
-  const check = () => (store.isAuth ? store.user.username : "new member");
   return (
     <main>
-      <h2>Hello {check()}!</h2>
-      <div>
-        <p>
+      <h2 className="main__title">Hello {store.isAuth ? store.user.username : "new member"}!</h2>
+      <div className="main__content">
+        <p className="main__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem architecto porro laborum repudiandae consectetur vitae
+          pariatur iure dolor mollitia quasi quod, maiores, eaque culpa? Ipsum
+          explicabo repudiandae facilis laborum ullam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem architecto porro laborum repudiandae consectetur vitae
+          pariatur iure dolor mollitia quasi quod, maiores, eaque culpa? Ipsum
+          explicabo repudiandae facilis laborum ullam.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem architecto porro laborum repudiandae consectetur vitae
+          pariatur iure dolor mollitia quasi quod, maiores, eaque culpa? Ipsum
+          explicabo repudiandae facilis laborum ullam.
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Exercitationem architecto porro laborum repudiandae consectetur vitae
           pariatur iure dolor mollitia quasi quod, maiores, eaque culpa? Ipsum
--- src/Pages/UserProfilePage.tsx
+++ src/Pages/UserProfilePage.tsx
@@ -2,6 +2,7 @@ import { FC, useContext } from 'react'
 import { Context } from "../index";
 import { Avatar } from '@mui/material';
 import { observer } from 'mobx-react-lite';
+import { Input, Button } from '@mui/material';
 import "../styles/userProfile.css"
 
 const UserProfilePage: FC = () => {
@@ -18,7 +19,7 @@ const UserProfilePage: FC = () => {
           fontSize: "50px"
         }}>{store.user.firstName}</Avatar>
         <ul className="profile__info">
-          {store.user.birthday ? <li className="profile__info__list">My Birthday: {store.user.birthday}</li> : null}
+          {store.user.birthday ? <li className="profile__info__list">My Birthday: {store.user.birthday.split('-').reverse().join('.')}</li> : null}
           {store.user.position ? <li className="profile__info__list">Position: {store.user.position}</li> : null}
           {store.user.department ? <li className="profile__info__list">Department: {store.user.department}</li> : null}
           {store.user.education ? <li className="profile__info__list">Education: {store.user.education}</li> : null}
@@ -47,6 +48,28 @@ const UserProfilePage: FC = () => {
         <p className="profile__about">
           {store.user.description ? `About me: ${store.user.description}` : "Nothing at now..."}
         </p>
+        <div className="profile__posts">
+          <form className="posts__create">
+            <Input type="file" sx={{
+              margin: "0 0 1rem 0",
+              width: "fit-content",
+              ":after": { borderBottom: "2px solid #bf4444" },
+              
+            }}/>
+            <Input type="text" placeholder="Tell us something" multiline={true} sx={{
+              margin: "0 0 1rem 0",
+              ":after": { borderBottom: "2px solid #bf4444" }
+            }} />
+            <Button variant="contained"        sx={{
+          margin: "2rem 2rem 1rem 2rem",
+          backgroundColor: "#bf4444",
+          ":hover": {
+            backgroundColor: "#bc6464",
+          },
+        }}>Create Post</Button>
+          </form>
+          <div className="posts__list"></div>
+        </div>
       </div>
     </div>
   )
