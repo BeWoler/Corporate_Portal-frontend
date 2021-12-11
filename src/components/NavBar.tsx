a25d9a63238d716a95c0e80dfd5069a5666acac1
BeWoler <bewoler.del@gmail.com>
2021-12-11 09:16:31 +0300

    edited navBar styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,49 +1,55 @@
 import "../styles/navBar.css";
-import { FC, useContext } from 'react';
+import { FC, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../index";
 import SettingsIcon from "@mui/icons-material/Settings";
-import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-import LogoutIcon from '@mui/icons-material/Logout';
-import PersonIcon from '@mui/icons-material/Person';
-import ChatIcon from '@mui/icons-material/Chat';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PersonIcon from "@mui/icons-material/Person";
+import ChatIcon from "@mui/icons-material/Chat";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ImportContactsIcon from "@mui/icons-material/ImportContacts";
 
 const NavBar: FC = () => {
   const { store } = useContext(Context);
-  if(!store.isAuth) {
+  const linkStyle = {
+    color: "#bf4444",
+    verticalAlign: "bottom",
+    marginRight: ".4rem",
+  };
+
+  if (!store.isAuth) {
     return null;
   }
   return (
     <nav>
       <ul className="nav__ul">
         <Link to={`${store.user.username}/profile`} className="nav__li">
-          <PersonIcon sx={{ color: "#bf4444", verticalAlign: "bottom", marginRight: ".4rem" }} />
+          <PersonIcon sx={linkStyle} />
           My Profile
         </Link>
         <Link to="friends" className="nav__li">
-          <ImportContactsIcon sx={{ color: "#bf4444", verticalAlign: "bottom", marginRight: ".4rem" }} />
+          <ImportContactsIcon sx={linkStyle} />
           Friends
         </Link>
         <Link to="board" className="nav__li">
-          <DashboardIcon sx={{ color: "#bf4444", verticalAlign: "bottom", marginRight: ".4rem" }} />
+          <DashboardIcon sx={linkStyle} />
           Board
         </Link>
         <Link to="chat" className="nav__li">
-          <ChatIcon sx={{ color: "#bf4444", verticalAlign: "bottom", marginRight: ".4rem" }} />
+          <ChatIcon sx={linkStyle} />
           Messages
         </Link>
         <Link to="users" className="nav__li">
-          <PeopleAltIcon sx={{ color: "#bf4444", verticalAlign: "bottom", marginRight: ".4rem" }} />
+          <PeopleAltIcon sx={linkStyle} />
           Users
         </Link>
         <Link to="settings" className="nav__li">
-          <SettingsIcon sx={{ color: "#bf4444", verticalAlign: "bottom", marginRight: ".4rem" }} />
+          <SettingsIcon sx={linkStyle} />
           Settings
         </Link>
         <Link to="/login" className="nav__li" onClick={() => store.logout()}>
-          <LogoutIcon sx={{ color: "#bf4444", verticalAlign: "bottom", marginRight: ".4rem" }} />
+          <LogoutIcon sx={linkStyle} />
           Logout
         </Link>
       </ul>
