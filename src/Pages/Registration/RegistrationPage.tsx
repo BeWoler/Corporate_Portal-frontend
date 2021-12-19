22528497d0df54efe391fe4da7980a74339e0134
BeWoler <bewoler.del@gmail.com>
2021-12-20 01:27:39 +0300

    edited registrPage & title

--- src/Pages/Registration/RegistrationPage.tsx
+++ src/Pages/Registration/RegistrationPage.tsx
@@ -4,6 +4,7 @@ import "./registrationForm.css";
 import { Button, Input } from "@mui/material";
 import { Link } from "react-router-dom";
 import { observer } from "mobx-react-lite";
+import Title from "../../components/Title/Title";
 
 const RegistrationPage: FC = () => {
   const [email, setEmail] = useState<string>("");
@@ -13,55 +14,60 @@ const RegistrationPage: FC = () => {
 
   const inputStyles = {
     margin: "1.5rem 1.3rem 0.8rem 1.3rem",
-    ":after": { borderBottom: "2px solid #bf4444" },
+    ":after": { borderBottom: "2px solid #534ED9" },
   };
   const btnStyles = {
     margin: "2rem 1.3rem 2rem 1.3rem",
-    backgroundColor: "#bf4444",
-    ":hover": { backgroundColor: "#bc6464" },
+    backgroundColor: "#534ED9",
+    ":hover": { backgroundColor: "#7673D9" },
   };
 
   return (
     <form className="registr__form">
-      <h2 className="form__h2">Join Us</h2>
-      <p className="error">{store.error}</p>
-      <Input
-        placeholder="Email"
-        type="text"
-        sx={inputStyles}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <Input
-        placeholder="Username"
-        type="text"
-        sx={inputStyles}
-        onChange={(e) => {
-          setUsername(e.target.value);
-        }}
-      />
-      <Input
-        placeholder="Password"
-        type="password"
-        sx={inputStyles}
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <Button
-        variant="contained"
-        sx={btnStyles}
-        onClick={async () => {
-          await store.registration(email, username, password);
-          store.checkAuth();
-        }}
-      >
-        Sign Up
-      </Button>
-      <div className="form__linkBox">
-        <span>Have an account?</span>
-        <Link to="/login">Sign In</Link>
+      <div className="title__box">
+        <Title />
+      </div>
+      <div className="form__box">
+        <h2 className="form__h2">Join Us</h2>
+        <p className="error">{store.error}</p>
+        <Input
+          placeholder="Email"
+          type="text"
+          sx={inputStyles}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <Input
+          placeholder="Username"
+          type="text"
+          sx={inputStyles}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <Input
+          placeholder="Password"
+          type="password"
+          sx={inputStyles}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <Button
+          variant="contained"
+          sx={btnStyles}
+          onClick={async () => {
+            await store.registration(email, username, password);
+            store.checkAuth();
+          }}
+        >
+          Sign Up
+        </Button>
+        <div className="form__linkBox">
+          <span>Have an account?</span>
+          <Link to="/login">Sign In</Link>
+        </div>
       </div>
     </form>
   );
