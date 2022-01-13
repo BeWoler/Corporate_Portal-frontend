a366e8d715ec7e5258719b6b7ff27568f78c7d90
BeWoler <bewoler.del@gmail.com>
2022-01-13 20:13:01 +0300

    added ui test for user profile

---
+++ cypress/integration/UserProfile.test/UserProfile.test.ts
@@ -0,0 +1,46 @@
+import { login } from "../../loginFunction";
+
+describe("UserProfile test", () => {
+  it("Has user profile", () => {
+    login();
+
+    cy.get("nav");
+    cy.get("nav a").eq(0).click();
+
+    cy.get(".profile__container").should("have.class", "profile__container");
+    cy.get(".profile__container")
+      .find(".profile__first__column")
+      .should("have.class", "profile__first__column");
+    cy.get(".profile__container")
+      .find(".profile__second__column")
+      .should("have.class", "profile__second__column");
+  });
+
+  it("User has avatar", () => {
+    cy.get(".MuiAvatar-root.MuiAvatar-square").should("have.text", "Test");
+  });
+
+  it("User has first name and last name", () => {
+    cy.get(".profile__name").should("have.class", "profile__name");
+  });
+
+  it("User has about data", () => {
+    cy.get(".profile__about").should("have.class", "profile__about");
+  });
+
+  it("User has form for create a post", () => {
+    cy.get("form.posts__create").should("have.class", "posts__create");
+  });
+
+  it("User has profile info", () => {
+    cy.get("ul.profile__info").should("have.class", "profile__info");
+  });
+
+  it("User has box with friends", () => {
+    cy.get("div.userFriends__container").should(
+      "have.class",
+      "userFriends__container"
+    );
+    cy.get("h3.userFriends__title").should("have.contain.text", "Friends");
+  });
+});
--- cypress/loginFunction.ts
+++ cypress/loginFunction.ts
@@ -1,9 +1,7 @@
 export const login = () => {
   cy.visit("/login");
 
-  cy.get('input[placeholder="Username"]')
-    .type("test")
-    .should("have.value", "test");
+  cy.get('input[placeholder="Username"]').type("test");
   cy.get('input[placeholder="Password"]').type("1234");
 
   cy.get("button").click();
