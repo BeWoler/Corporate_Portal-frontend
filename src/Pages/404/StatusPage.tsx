799a0841e0cf415dfc907d6474ba71e172bc8316
BeWoler <bewoler.del@gmail.com>
2022-01-13 03:47:02 +0300

    added 404 page

---
+++ src/Pages/404/StatusPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const StatusPage: FC = () => {
+  return (
+    <div className="not__found">
+      <span>404</span>
+      <span>Not Found</span>
+    </div>
+  );
+};
+
+export default StatusPage;
--- src/hoc/RequireAdminAuth.tsx
+++ src/hoc/RequireAdminAuth.tsx
@@ -1,16 +1,12 @@
 import { useContext } from "react";
 import { Context } from "../index";
+import StatusPage from "../Pages/404/StatusPage";
 
 const RequireAdminAuth = ({ children }: { children: JSX.Element }) => {
   const { store } = useContext(Context);
 
   if (!store.isAdmin) {
-    return (
-      <div className="not__found">
-        <span>404</span>
-        <span>Not Found</span>
-      </div>
-    );
+    return <StatusPage />;
   }
   return children;
 };
